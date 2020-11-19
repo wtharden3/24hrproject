3015c8b46e9b160f3c3dbdc6a8127389deb11673
Whitney Harden <wtharden320@gmail.com>
2020-11-19 15:48:59 -0500

    App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import Header from './components/site/Header';
+import Footer from './components/site/Footer';
+import Sidebar from './components/site/Sidebar';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Now
-        </a>
-      </header>
+      <Header />
+      <Router>
+        <Sidebar />
+      </Router>
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React Now
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
