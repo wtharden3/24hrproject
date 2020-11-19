8354d22766e4cb13a0c05980433e35293a85d2e6
Khushman <khushman.jauhal@gmail.com>
2020-11-19 14:37:02 -0500

    update

---
+++ src/components/concepts/FunctionalComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FunctionalComponentDemo = function () {
+  return (
+    <div className="main">
+      <div className="mainDiv">Hello React</div>
+    </div>
+  );
+};
+
+export default FunctionalComponentDemo;
