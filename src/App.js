af71da6d6d0d27179f78c2a00e6de450bd5c81c4
Whitney Harden <wtharden320@gmail.com>
2020-11-19 18:52:58 -0500

    app.js shows after khush merge

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
@@ -6,36 +5,15 @@ import Header from './components/site/Header';
 import Footer from './components/site/Footer';
 import Sidebar from './components/site/Sidebar';
 import { BrowserRouter as Router } from 'react-router-dom';
-=======
-import logo from "./logo.svg";
-import "./App.css";
->>>>>>> khushman
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Header />
       <Router>
         <Sidebar />
       </Router>
       <Footer />
-=======
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Now Now
-        </a>
-      </header>
->>>>>>> khushman
     </div>
   );
 }
