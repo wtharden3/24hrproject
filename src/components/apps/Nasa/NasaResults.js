8760cf62474c5d269b2c458a718e9040befb3ab4
Whitney Harden <wtharden320@gmail.com>
2020-11-20 09:58:19 -0500

    Nasa.js and Nasa.css

--- src/components/apps/Nasa/NasaResults.js
+++ src/components/apps/Nasa/NasaResults.js
@@ -1,13 +1,11 @@
-const NasaResults = (props) => {
-  return(
+import './Nasa.css'
+
+const NasaResults = props => {
+  return (
     <div>
-      {props.results.map(results => {
-        return(
-          <div>{results}</div>
-        )
-      })}
+      {props.url ? <img className="img-width" src={props.url} alt="location" /> : <h1>nothing is coming up 🤬</h1>}
     </div>
-  )
-}
+  );
+};
 
 export default NasaResults;
